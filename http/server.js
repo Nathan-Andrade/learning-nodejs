5a77d3328f1e8a77b51b8a7b438ccd7d995145de
Nathan Andrade <nathandulino2003@gmail.com>
2020-09-28 23:24:10 -0300

    server

--- http/server.js
+++ http/server.js
@@ -6,13 +6,16 @@ http.createServer((req, res) => {
 
   const file = req.url === '/' ? 'index.html' : req.url
   const filePath = path.join(__dirname, 'public', file)
+  const extname = path.extname(filePath)
 
-  res.end('Chegueiiii')
+  const allowedFileTypes = ['.html', '.css', '.js']
+  const allowed = allowedFileTypes.find(item => item == extname)
 
   fs.readFile(
     filePath,
     (err, content) => {
       if (err) throw err
+
       res.end(content)
     }
   )
